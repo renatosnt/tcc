187e66fe94d25f9234a5a36e0c5c19d23d972313
Renato Santos <renatossnt@gmail.com>
2025-06-03 17:24:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Fontes de dados:
+Formulario
+SIMI
+CNPq
+FAPEMIG (Projetos financiados, editais, laboratorios vinculados)
---
+++ teste.py
@@ -0,0 +1,24 @@
+  'Cadastro aprovado?',# Ignorar
+  'Você concorda em divulgar as informações inseridas neste formulário em estudos sobre o ecossistema de inovação de Minas Gerais e na plataforma do SIMI?', # Ignorar
+  'Qual o nome da sua organização?',# nome_unidade
+  'Em qual cidade está a sede da organização?',# cidade_unidade; usar junto com UF para derivar latitude e longitude
+  'Em qual estado (UF) está a sede da organização?',# uf
+  'Qual o link com o logo da organização?',# TODO: novo parametro url_logo
+  'Descrição da organização',# TODO: interpretar
+  'Qual o site da organização?',# site
+  'Qual o melhor e-mail para contato?',# email_contato
+  'Em qual categoria sua organização se enquadra?', # TODO: interpretar
+  'Qual fase de investimentos que está captando?', # TODO: interpretar
+  'Qual(is) incentivo(s) fiscal(is) o município possui para o desenvolvimento de empresas de base tecnológica?', # TODO interpretar
+  'Qual departamento (secretaria, subsecretaria, diretoria, assessoria, etc) é responsável pela pauta/temática de Ciência, Tecnologia e Inovação?', # TODO interpretar
+  'Qual o tipo de organização?', # TODO interpretar
+  'Caso sua organização possua um setor ou pessoa responsável por divulgação científica, informe o contato', # TODO interpretar (possívelmente o ponto focal)
+  'A organização possui um NIT (Núcleo de Inovação Tecnológica) ou departamento responsável por propriedade intelectual e transferências tecnológicas?', # TODO interpretar
+  'Quais são as principais áreas do conhecimento em que a organização possui expertise?', # TODO interpretar (possivelmente areas de pesquisa ou area_de_conhecimento)
+  'Qual a tese de investimento da organização?', # TODO interpretar
+  'Em quantas startups a organização já investiu?', # TODO interpretar
+  'Segmento', # TODO interpretar
+  'Programas de desenvolvimento', # TODO interpretar
+  'Modelo de negócio', # TODO interpretar
+  'Tecnologia disruptiva', # TODO interpretar
+  'Segmentação de clientes', # TODO interpretar
